fe1c9788d3a0e1b536d9e8d39733e4cee127c9d6
Mateusz Suder <matisu999@gmail.com>
2022-04-04 17:53:56 +0200

    add theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,27 @@
 import React from 'react';
 import {getAccessToken, UserProvider} from '@auth0/nextjs-auth0';
 import GraphQLProvider from "../components/GraphQLProvider";
+import {createTheme, ThemeProvider} from "@mui/system";
+import {purple} from "@mui/material/colors";
+
+const theme = createTheme({
+	palette: {
+		primary: {
+			main: purple[500],
+		},
+		secondary: {
+			main: '#f44336',
+		},
+	},
+});
 
 export default function App({ Component, pageProps }) {
 	return (
 		<UserProvider>
 			<GraphQLProvider>
-				<Component {...pageProps} />
+				<ThemeProvider theme={theme}>
+					<Component {...pageProps} />
+				</ThemeProvider>
 			</GraphQLProvider>
 		</UserProvider>
 	);
